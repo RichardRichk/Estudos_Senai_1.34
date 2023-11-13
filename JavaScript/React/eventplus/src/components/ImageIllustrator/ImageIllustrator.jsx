b6fe6195d043e6d23a80439aad89b57f5a42f4a4
Richard Fernandes Passarelli <45811737874@info.local>
2023-11-13 07:55:00 -0300

    Criando pagina TipoEventos

--- JavaScript/React/eventplus/src/components/ImageIllustrator/ImageIllustrator.jsx
+++ JavaScript/React/eventplus/src/components/ImageIllustrator/ImageIllustrator.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import './ImageIllustrator.css'
-import tipoEventoImage from '../../assets/images/tipo-evento.svg'
-import eventoImage from
+import './ImageIllustrator.css';
+import tipoEventoImage from '../../assets/images/tipo-evento.svg';
+import eventoImage from '../../assets/images/eventoImage';
 
 const ImageIllustrator = ({altText, imageName, additionalClass}) => {
 
