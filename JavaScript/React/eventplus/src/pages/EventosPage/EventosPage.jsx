992cb5bfdccaaa896ef45129a8dc5aa48e5ed682
Richard Fernandes Passarelli <45811737874@info.local>
2023-11-23 10:25:29 -0300

    Realizando pagina Eventos, necessario realizar Metodo handle Update, handleSubmit e fazer Select para tipoEventos e Instituicao

--- JavaScript/React/eventplus/src/pages/EventosPage/EventosPage.jsx
+++ JavaScript/React/eventplus/src/pages/EventosPage/EventosPage.jsx
@@ -5,8 +5,9 @@ import MainContent from '../../components/Main/MainContent';
 import Container from '../../components/Container/Container';
 import ImageIllustrator from '../../components/ImageIllustrator/ImageIllustrator';
 import { Input, Button, Select } from '../../components/FormComponents/FormComponents';
-import api, {eventsResource} from "../../Services/Service.js"
+import api, { eventsResource } from "../../Services/Service.js"
 import TableEv from './TableEv/TableEv';
+import Notification from '../../components/Notification/Notification';
 
 import EventoImage from '../../assets/images/evento.svg'
 
@@ -24,12 +25,18 @@ const EventosPage = () => {
 
     const [tipoEvento, setTipoEvento] = useState("");
 
+    const [instituicao, setInstituicao] = useState("");
+
     const [dataEvento, setDataEvento] = useState("");
 
-    const [eventos, setEventos] = useState([ ]); //array
+    const [idEvento, setIdEvento] = useState(null);
 
-    useEffect(()=>{
-        
+    const [eventos, setEventos] = useState([]); //array
+
+
+
+    useEffect(() => {
+
         //define a chamada em nossa api
         async function loadEvents() {
             // setShowSpinner(true)
@@ -43,7 +50,7 @@ const EventosPage = () => {
                 console.log("Erro na api");
                 console.log(error);
             }
-            
+
             // setShowSpinner(false)
         }
 
@@ -51,46 +58,138 @@ const EventosPage = () => {
         loadEvents();
     }, [])
 
-    //MOLDA O ARRAY OPTIONS
-    // function dePara(options) {
-    //     const arrayOptions = [];
-    //     return options.forEach(element => {
-    //         arrayOptions.push({calue: element.idTipoEvento, text: element.titulo});
-    //     })
-    //     return arrayOptions;
-    // }
-
     async function updateAPI() {
         const buscaEventos = await api.get(eventsResource);
 
         setEventos(buscaEventos.data); //atualiza a variavel e roda o useState novamente(que da um get na api)       
     }
 
-    async function showUpdateForm(idElement){
+    async function handleSubmit(e) {
+        e.preventDefault();
+        if (nome.trim().length < 3) {
+            setNotifyerUser({
+                titleNote: "Erro no Titulo",
+                textNote: `${nome} contem menos de 3 caracteres`,
+                imgIcon: "warning",
+                imgAlt: "Imagem de ilustracao de erro, Cuidado!",
+                showMessage: true
+            })
+        }
+        else try {
+            const promiseRetorno = await api.post(eventsResource, { nome: nome, descricao: descricao, tipoEvento: tipoEvento, dataEvento: dataEvento })
+
+            updateAPI();
+
+            setNotifyerUser({
+                titleNote: "Sucesso",
+                textNote: `${nome} cadastrado com sucesso`,
+                imgIcon: "success",
+                imgAlt: "Imagem de ilustracai de sucessi.moca segurando um balao com simbolo de confirmacao ok",
+                showMessage: true
+            });
+        } catch (error) {
+            setNotifyerUser({
+                titleNote: "Erro na Aplicacao",
+                textNote: `Nao foi possivel cadastrar ${nome}`,
+                imgIcon: "danger",
+                imgAlt: "Imagem de ilustracai de erro, Warning!",
+                showMessage: true
+            });
+        }
     }
 
-    async function handDelete(idElement) {
+    async function handleUpdate(e) {
+        e.preventDefault();
         
+        try {
+
+            const promiseRetorno = await api.put(`${eventsResource}/${idEvento}`, {nome: nome, descricao: descricao, tipoEvento: tipoEvento, dataEvento: dataEvento})
+
+            if (promiseRetorno.status === 204) {
+                //Notificar usuario
+                setNotifyerUser({
+                    titleNote: "Sucesso",
+                    textNote: `Atualizado com sucesso`,
+                    imgIcon: "success",
+                    imgAlt: "Imagem de ilustracai de sucessi.moca segurando um balao com simbolo de confirmacao ok",
+                    showMessage: true
+                });
+
+                //Atualizar dados
+                const retorno = await api.get(eventsResource);
+                setEventos(retorno.data)
+
+                //Sair da tela de cadastro
+                editActionAbort();
+
+            }
+
+        } catch (error) {
+            setNotifyerUser({
+                titleNote: "Erro na Aplicacao",
+                textNote: `Nao foi possivel atualizar ${error}`,
+                imgIcon: "danger",
+                imgAlt: "Imagem de ilustracai de erro, Warning!",
+                showMessage: true
+            });
+        }
+    }
+
+    function editActionAbort() {
+        setFrmEdit(false);
+        setNome(""); //Reseta as variaveis
+        setDescricao("");
+        setTipoEvento("");
+        setInstituicao("");
+        setDataEvento("");
+        setIdEvento(null); //Reseta as variaveis
+    }
+
+    async function showUpdateForm(idElement) {
+        setFrmEdit(true);
+
+        setIdEvento(idElement);
+
+        try {
+
+            const retorno = await api.get(`${eventsResource}/${idElement}`);
+            setNome(retorno.data.nomeEvento);
+            setDescricao(retorno.data.descricao);
+            setDataEvento(retorno.data.dataEvento);
+
+        } catch (error) {
+            setNotifyerUser({
+                titleNote: "Erro",
+                textNote: `Erro na API`,
+                imgIcon: "warning",
+                imgAlt: "Imagem de ilustracai de erro. Warning",
+                showMessage: true
+            });
+        }
+    }
+
+    async function handDelete(idElement, nomeElement) {
+
         //Se nao confirma a exclusao, cancela a acao
-        if (window.confirm(`Deseja apagar ?`)) {
-            
+        if (window.confirm(`Deseja apagar ${nomeElement} ?`)) {
+
             try {
                 const promiseRetorno = await api.delete(`${eventsResource}/${idElement}`);
-                if (promiseRetorno.status == 204) {
+                if (promiseRetorno.status === 204) {
 
                     //Funcao chamando API para atualizar
                     updateAPI();
 
                     setNotifyerUser({
                         titleNote: "Sucesso",
-                        textNote: ` excluido com sucesso`,
+                        textNote: `${nomeElement} excluido com sucesso`,
                         imgIcon: "success",
                         imgAlt: "Imagem de ilustracai de sucessi.moca segurando um balao com simbolo de confirmacao ok",
                         showMessage: true
                     });
                 }
-    
-            } catch (error){
+
+            } catch (error) {
                 setNotifyerUser({
                     titleNote: "Erro",
                     textNote: `Erro na API`,
@@ -101,15 +200,18 @@ const EventosPage = () => {
             }
 
         }
-        
+
     }
 
     return (
         <>
+
+            <Notification {...notifyUser} setNotifyUser={setNotifyerUser} />
+
             <MainContent>
                 <Container>
                     <div className='cadastro-evento__box'>
-                        <Title titleText={"Cadastro de Eventos"}/>
+                        <Title titleText={"Cadastro de Eventos"} />
 
                         <ImageIllustrator
                             imageRender={EventoImage}
@@ -118,38 +220,121 @@ const EventosPage = () => {
                         <form
                             className='ftipo-evento'
                         >
-                        <Input
-                            id={"Nome"}
-                            placeholder={"Nome"}
-                            name={"nome"}
-                            type={"text"}
-                            required={"required"}
-                        />
-                        <Input
-                            id={"Descricao"}
-                            placeholder={"Descricao"}
-                            name={"descricao"}
-                            type={"text"}
-                            required={"required"}
-                        />
-                        <select name="" id="">
+                            {
+                                !frmEdit ?
+                                    <>
+                                        (<p>Tela de Cadastro</p>)
+                                        <Input
+                                            id={"Nome"}
+                                            placeholder={"Nome"}
+                                            name={"nome"}
+                                            type={"text"}
+                                            required={"required"}
+                                            value={nome}
+                                            manipulationFunction={(e) => {
+                                                setNome(e.target.value);
+                                            }}
+                                        />
+                                        <Input
+                                            id={"Descricao"}
+                                            placeholder={"Descricao"}
+                                            name={"descricao"}
+                                            type={"text"}
+                                            required={"required"}
+                                            value={descricao}
+                                            manipulationFunction={(e) => {
+                                                setDescricao(e.target.value);
+                                            }}
+                                        />
+                                        <select name="" id="">
 
-                        </select>
-                        <Input
-                            id={"DataEvento"}
-                            placeholder={"dd/mm/aaaa"}
-                            name={"dataEvento"}
-                            type={"Date"}
-                            required={"required"}
-                        />
+                                        </select>
+                                        <Input
+                                            id={"DataEvento"}
+                                            placeholder={"dd/mm/aaaa"}
+                                            name={"dataEvento"}
+                                            type={"Date"}
+                                            required={"required"}
+                                            value={dataEvento}
+                                            manipulationFunction={(e) => {
+                                                setDataEvento(e.target.value);
+                                            }}
+                                        />
 
-                        <Button
-                            className = 'btn-cadastrar'
-                            textButton="Cadastrar"
-                            id="Cadastrar"
-                            name="cadastrar"
-                            type="submit"
-                        />
+                                        <Button
+                                            className='btn-cadastrar'
+                                            textButton="Cadastrar"
+                                            id="Cadastrar"
+                                            name="cadastrar"
+                                            type="submit"
+                                        />
+                                    </>
+
+
+                                    :
+
+
+                                    //Edit
+                                    <>
+                                        (<p>Tela Edicao</p>)
+
+                                        <Input
+                                            id={"Nome"}
+                                            placeholder={"Nome"}
+                                            name={"nome"}
+                                            type={"text"}
+                                            required={"required"}
+                                            value={nome}
+                                            manipulationFunction={(e) => {
+                                                setNome(e.target.value);
+                                            }}
+                                        />
+                                        <Input
+                                            id={"Descricao"}
+                                            placeholder={"Descricao"}
+                                            name={"descricao"}
+                                            type={"text"}
+                                            required={"required"}
+                                            value={descricao}
+                                            manipulationFunction={(e) => {
+                                                setDescricao(e.target.value);
+                                            }}
+                                        />
+                                        <select name="" id="">
+
+                                        </select>
+                                        <Input
+                                            id={"DataEvento"}
+                                            placeholder={"dd/mm/aaaa"}
+                                            name={"dataEvento"}
+                                            type={"Date"}
+                                            required={"required"}
+                                            value={dataEvento}
+                                            manipulationFunction={(e) => {
+                                                setDataEvento(e.target.value);
+                                            }}
+                                        />
+                                        <div className='buttons-editbox'>
+                                            <Button
+                                                textButton="Atualizar"
+                                                id="Atualizar"
+                                                name="Atualizar"
+                                                type="submit"
+                                                manipulationFunction={handleUpdate}
+                                                additionalClass="button-component--midle"
+                                            />
+
+                                            <Button
+                                                textButton="Cancelar"
+                                                id="Cancelar"
+                                                name="Cancelar"
+                                                type="button"
+                                                manipulationFunction={editActionAbort}
+                                                additionalClass="button-component--midle"
+                                            />
+                                        </div>
+                                    </>
+                            }
                         </form>
                     </div>
 
@@ -158,10 +343,12 @@ const EventosPage = () => {
                 <section className='lista-eventos-section'>
                     <Container>
 
-                        <Title titleText={"lista de eventos"} color="white"/>
+                        <Title titleText={"lista de eventos"} color="white" />
 
                         <TableEv
                             dados={eventos}
+                            fnDelete={handDelete}
+                            fnUpdate={showUpdateForm}
                         />
 
                     </Container>
--- JavaScript/React/eventplus/src/pages/EventosPage/TableEv/TableEv.jsx
+++ JavaScript/React/eventplus/src/pages/EventosPage/TableEv/TableEv.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import "./TableEv.css"
 import editPen from '../../../assets/images/edit-pen.svg';
-import trashDelete from '../../../assets/images/trash-delete.svg'
+import trashDelete from '../../../assets/images/trash-delete.svg';
+import { Tooltip } from 'react-tooltip';
 
-const TableEv = ({dados}) => {
+const TableEv = ({ dados, fnDelete = null, fnUpdate = null }) => {
     return (
         <table className='table-data'>
 
@@ -31,7 +32,17 @@ const TableEv = ({dados}) => {
                             </td>
 
                             <td className="table-data__data table-data__data--big">
-                                {event.descricao}
+
+                                <p 
+                                    className='event-card__description'
+                                    data-tooltip-id={event.idEvento}
+                                    data-tooltip-content={event.descricao}
+                                    data-tooltip-place="top"
+                                >
+                                    <Tooltip id={event.idEvento} className="custom-tooltip" />
+                                    {event.descricao.substr(0, 15)}...
+                                </p>
+
                             </td>
 
                             <td className="table-data__data table-data__data--big">
@@ -47,6 +58,9 @@ const TableEv = ({dados}) => {
                                 className="table-data__icon" 
                                 src={editPen} alt="" 
                                 idEvento = {event.idEvento}
+                                onClick={() => {
+                                    fnUpdate(event.idEvento)
+                                }}
                                 />
                             </td>
 
@@ -56,6 +70,9 @@ const TableEv = ({dados}) => {
                                     className="table-data__icon" 
                                     src={trashDelete} alt=""
                                     idevento = {event.idEvento} 
+                                    onClick={() => {
+                                        fnDelete(event.idEvento, event.nomeEvento)
+                                    }}
                                 />
                             </td>
                         </tr>
